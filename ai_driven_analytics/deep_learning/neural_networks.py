3dd3c95dd64ce378cec2c3e2a477863067e83277
KOSASIH <kosasihg88@gmail.com>
2024-07-10 16:14:56 +0700

    Create neural_networks.py

---
+++ ai_driven_analytics/deep_learning/neural_networks.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+
+class NeuralNetworks:
+    def __init__(self, input_shape, num_classes):
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.model = self.build_model()
+
+    def build_model(self):
+        model = tf.keras.models.Sequential([
+            Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape),
+            MaxPooling2D((2, 2)),
+            Conv2D(64, (3, 3), activation='relu'),
+            MaxPooling2D((2, 2)),
+            Conv2D(128, (3, 3), activation='relu'),
+            MaxPooling2D((2, 2)),
+            Flatten(),
+            Dense(128, activation='relu'),
+            Dense(self.num_classes, activation='softmax')
+        ])
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        return model
