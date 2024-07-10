67a6e8f3c7b0b782e5697a361a8abdf28594a5eb
KOSASIH <kosasihg88@gmail.com>
2024-07-10 16:44:41 +0700

    Create self_sovereign_identity.py

---
+++ blockchain_based_data_management/decentralized_identity_management/self_sovereign_identity.py
@@ -0,0 +1,20 @@
+import json
+from indy import IndyError, wallet
+
+class SelfSovereignIdentity:
+    def __init__(self, wallet_name, wallet_key):
+        self.wallet_name = wallet_name
+        self.wallet_key = wallet_key
+        self.wallet = wallet.open_wallet(self.wallet_name, self.wallet_key)
+
+    def create_did(self, did_method, did_key):
+        did, verkey = wallet.create_did(self.wallet, did_method, did_key)
+        return did, verkey
+
+    def store_credential(self, cred_def_id, cred_values):
+        cred_req = wallet.store_credential(self.wallet, cred_def_id, cred_values)
+        return cred_req
+
+    def get_credential(self, cred_id):
+        cred = wallet.get_credential(self.wallet, cred_id)
+        return cred
