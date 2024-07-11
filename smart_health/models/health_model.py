81bedc8b678a614ef821c46574252a9b2494d49c
KOSASIH <kosasihg88@gmail.com>
2024-07-11 16:19:35 +0700

    Update health_model.py

--- smart_health/models/health_model.py
+++ smart_health/models/health_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 class HealthModel:
     def __init__(self):
@@ -11,3 +12,7 @@ class HealthModel:
 
     def predict(self, X):
         return self.model.predict(X)
+
+    def evaluate(self, X, y):
+        y_pred = self.model.predict(X)
+        return mean_squared_error(y, y_pred)
