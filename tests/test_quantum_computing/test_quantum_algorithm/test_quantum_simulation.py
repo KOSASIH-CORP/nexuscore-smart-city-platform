7cd9a4884fe142c41cbc5402132ad9431cbf1616
KOSASIH <kosasihg88@gmail.com>
2024-07-11 11:21:07 +0700

    Create test_quantum_simulation.py

---
+++ tests/test_quantum_computing/test_quantum_algorithm/test_quantum_simulation.py
@@ -0,0 +1,7 @@
+import unittest
+from quantum_computing.quantum_algorithm.quantum_simulation import QuantumSimulation
+
+class TestQuantumSimulation(unittest.TestCase):
+    def test_quantum_simulation(self):
+        # implement test logic here
+        pass
