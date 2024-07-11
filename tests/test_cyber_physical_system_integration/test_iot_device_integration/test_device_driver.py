7f9e74225656f521e2634e64d1165ba386093bf6
KOSASIH <kosasihg88@gmail.com>
2024-07-11 11:18:57 +0700

    Create test_device_driver.py

---
+++ tests/test_cyber_physical_system_integration/test_iot_device_integration/test_device_driver.py
@@ -0,0 +1,7 @@
+import unittest
+from cyber_physical_system_integration.iot_device_integration.device_driver import DeviceDriver
+
+class TestDeviceDriver(unittest.TestCase):
+    def test_device_driver(self):
+        # implement test logic here
+        pass
