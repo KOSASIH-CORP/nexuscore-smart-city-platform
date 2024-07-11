74387cbd6d80432573a80cd0ba3c05593405a4c3
KOSASIH <kosasihg88@gmail.com>
2024-07-11 11:18:13 +0700

    Create test_symmetric_encryption.py

---
+++ tests/test_blockchain_based_data_management/test_data_encryption/test_symmetric_encryption.py
@@ -0,0 +1,7 @@
+import unittest
+from blockchain_based_data_management.data_encryption.symmetric_encryption import SymmetricEncryption
+
+class TestSymmetricEncryption(unittest.TestCase):
+    def test_symmetric_encryption(self):
+        # implement test logic here
+        pass
