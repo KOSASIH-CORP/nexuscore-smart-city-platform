49a2fe60ffa0e7abc24134a2a2adacb92a7d7740
KOSASIH <kosasihg88@gmail.com>
2024-07-11 11:17:09 +0700

    Create test_convolutional_neural_networks.py

---
+++ tests/test_ai_driven_analytics/test_machine_learning/test_neural_networks/test_convolutional_neural_networks.py
@@ -0,0 +1,7 @@
+import unittest
+from ai_driven_analytics.machine_learning.neural_networks.convolutional_neural_networks import ConvolutionalNeuralNetwork
+
+class TestConvolutionalNeuralNetwork(unittest.TestCase):
+    def test_convolutional_neural_network(self):
+        # implement test logic here
+        pass
