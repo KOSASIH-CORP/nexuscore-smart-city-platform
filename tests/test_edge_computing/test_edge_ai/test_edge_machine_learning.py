162129e114fc2b52e4c93bf7e899ec72756949c6
KOSASIH <kosasihg88@gmail.com>
2024-07-11 11:20:08 +0700

    Create test_edge_machine_learning.py

---
+++ tests/test_edge_computing/test_edge_ai/test_edge_machine_learning.py
@@ -0,0 +1,7 @@
+import unittest
+from edge_computing.edge_ai.edge_machine_learning import EdgeMachineLearning
+
+class TestEdgeMachineLearning(unittest.TestCase):
+    def test_edge_machine_learning(self):
+        # implement test logic here
+        pass
