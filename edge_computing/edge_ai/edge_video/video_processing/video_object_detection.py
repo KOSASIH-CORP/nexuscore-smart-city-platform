69369a26978e93ec6b2f2e06f45773f78877dfe9
KOSASIH <kosasihg88@gmail.com>
2024-07-11 14:24:56 +0700

    Create video_object_detection.py

---
+++ edge_computing/edge_ai/edge_video/video_processing/video_object_detection.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+class VideoObjectDetection:
+    def __init__(self, video_data):
+        self.video_data = video_data
+
+    def detect_objects(self):
+        # implement object detection logic here
+        pass
+
+    def track_objects(self):
+        # implement object tracking logic here
+        pass
+
+    def classify_objects(self):
+        # implement object classification logic here
+        pass
